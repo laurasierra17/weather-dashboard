51e24eefaaf6a2e7c791510b90485563290283ad
laurasierra17 <laura.sierra17@gmail.com>
2022-07-04 20:58:09 -0400

    functionality to show current weather info of inputted city

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var apiKey = "f68779a05e2dffd6da000fc8655e5f07";
 var searchBtn = $(".search-btn");
 // Input in the search section
 var searchInput = $(".search-input");
+// Point to previous search history section
+var prevSearch = $(".results-section");
 
 // Data used to display information of the weather
 var temp;
@@ -10,31 +12,60 @@ var wind;
 var humidity;
 var uvIndex;
 
+// Create dashboard with the information gathered
+function generateDashboard(temp, wind, humidity, uvIndex, cityInput) {
+    var dashboard = $(".dashboard");
+    // Card container
+    var cardContainer = $('<div class="card text-bg-dark mb-3">').css("max-width", "18rem");
+    // Card body; capitalize first letter
+    var cardBody = $('<div class="card-body">');
+    var cardTitle = $('<h3 class="card-title">').text(cityInput).css("text-transform", "capitalize");
+    cardBody.append(cardTitle)
+    var tempText = $('<p class="card-text">').text("Temp: " + temp + "°F");
+    cardBody.append(tempText);
+    var windText = $('<p class="card-text">').text("Wind: " + wind + " MPH");
+    cardBody.append(windText);
+    var humidityText = $('<p class="card-text">').text("Humidity: " + humidity + " %");
+    cardBody.append(humidityText);
+    var uvIndexText = $('<p class="card-text">').text("UV Index: " + uvIndex);
+    cardBody.append(uvIndexText);
+    cardContainer.append(cardBody);
+    
+    
+    dashboard.append(cardContainer);
+}
+
 // Fetch data from api when button is clicked to populate dashboard
-function fetchAPI(lat, lon) {    
+function fetchAPI(lat, lon, cityInput) {    
     // API call
     var apiCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude={part}&appid=${apiKey}`
     fetch(apiCall)
     .then(response => response.json())
     .then(data => {
+        // Retrieve data needed for the dashboard
         temp = data.current.temp;
         wind = data.current.wind_speed;
         humidity = data.current.humidity;
         uvIndex = data.current.uvi;
+
+        // Call function to generate dashboard
+        generateDashboard(temp, wind, humidity, uvIndex, cityInput);
     });
 }
 
 // Get latitude and longitude for city entered
 function fetchGeocode() {
-    var apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput.val()}&limit=5&appid=${apiKey}`
-    
+    var cityInput = searchInput.val();
+    var apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=${apiKey}`
+
     fetch(apiCall)
     .then(response => response.json())
     .then(data => {
         // To store each input's latitude and longitud coordinates
-        var lat = data[0].lat.toFixed(2);
-        var lon = data[0].lon.toFixed(2);
-        fetchAPI(lat, lon)
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+
+        fetchAPI(lat, lon, cityInput)
     })
 }
 
