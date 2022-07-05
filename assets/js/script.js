a2368547efa53b31571451953aa4d6b234042bb3
laurasierra17 <laura.sierra17@gmail.com>
2022-07-04 22:23:18 -0400

    display forecast for next 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ var searchBtn = $(".search-btn");
 var searchInput = $(".search-input");
 // Point to previous search history section
 var prevSearch = $(".results-section");
+// Point to section for the 5-day forecast
+var forecastSection = $(".forecast");
 
 // Data used to display information of the weather
 var temp;
@@ -12,11 +14,55 @@ var wind;
 var humidity;
 var uvIndex;
 
+function miniCard(day) {
+    // <div class="col-2 card border-warning mb-3" style="max-width: 18rem;">
+    //     <div class="card-header">Header</div>
+    //     <div class="card-body">
+    //         <h5 class="card-title">Warning card title</h5>
+    //         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //     </div>
+    // </div>
+
+    temp = day.main.temp;
+    wind = day.wind.speed;
+    humidity = day.main.humidity;
+
+    // Column container
+    var col = $('<div class="card border-warning mb-3">').css("max-width", "18rem");
+    // Card body
+    var cardBody = $('<div class="card-body">');
+    var tempText = $('<p class="card-text">').text("Temp: " + temp + "°F");
+    cardBody.append(tempText);
+    var windText = $('<p class="card-text">').text("Wind: " + wind + " MPH");
+    cardBody.append(windText);
+    var humidityText = $('<p class="card-text">').text("Humidity: " + humidity + " %");
+    cardBody.append(humidityText);
+
+    col.append(cardBody);
+    forecastSection.append(col);
+}
+
+// Create dashboard for the 5-day forecast
+function generate5DayDashboard(lat, lon) {
+    var apiCall = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+    fetch(apiCall)
+    .then(response => response.json())
+    .then(data => {
+        for (var i = 0; i < data.list.length; ) {
+            // Generate a card for each future forecast
+            miniCard(data.list[i]);
+            if (i === 0) i += 7;
+            else i += 8;
+        }
+    });
+}
+
 // Create dashboard with the information gathered
 function generateMainDashboard(temp, wind, humidity, uvIndex, cityInput) {
     var dashboard = $(".dashboard");
     // Card container
-    var cardContainer = $('<div class="card text-bg-dark mb-3">').css("max-width", "18rem");
+    var cardContainer = $('<div class="col-12 card text-bg-dark mb-3">').css("max-width", "18rem");
     // Card body; capitalize first letter
     var cardBody = $('<div class="card-body">');
     var cardTitle = $('<h3 class="card-title">').text(cityInput).css("text-transform", "capitalize");
@@ -35,21 +81,6 @@ function generateMainDashboard(temp, wind, humidity, uvIndex, cityInput) {
     dashboard.append(cardContainer);
 }
 
-// Create dashboard for the 5-day forecast
-function generate5DayDashboard(lat, lon) {
-    var apiCall = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-
-    fetch(apiCall)
-    .then(response => response.json())
-    .then(data => {
-        for (var i = 0; i < data.length; ) {
-            // Generate a card for each future forecast
-            miniCard(data[i]);
-            if (i === 0) i += 7
-            else i += 8
-        }
-    });
-}
 
 // Fetch data from api when button is clicked to populate dashboard
 function fetchAPI(lat, lon, cityInput) {    
